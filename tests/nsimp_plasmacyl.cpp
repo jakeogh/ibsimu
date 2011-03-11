52fe06acdf59fa5dfcf012eb0a35908307c6e08b
Taneli Kalvas <taneli.kalvas@jyu.fi>
2011-03-11 19:29:54 +0200

    Searching working nsimp test parameters.

--- tests/nsimp_plasmacyl.cpp
+++ tests/nsimp_plasmacyl.cpp
@@ -17,7 +17,7 @@
 #include "geometry.hpp"
 #include "func_solid.hpp"
 #include "epot_efield.hpp"
-#include "vectorfield.hpp"
+#include "meshvectorfield.hpp"
 #include "ibsimu.hpp"
 #include "error.hpp"
 #include "particlediagplotter.hpp"
@@ -30,6 +30,8 @@ using namespace std;
 
 bool solid1( double x, double y, double z )
 {
+    if( x <= 0.0 && y <= 0.005 )
+	return( false );
     return( x <= 0.004 && y >= 0.0030 && 
 	    y >= 4.8*x - 0.003 );
 }
@@ -52,17 +54,17 @@ void test( int argc, char **argv )
 {
     // 30x20 mm geometry with 0.05 mm mesh size
     //Geometry geom( MODE_CYL, Int3D(601,401,1), Vec3D(0,0,0), 0.00005 );
-    Geometry geom( MODE_CYL, Int3D(301,201,1), Vec3D(0,0,0), 0.0001 );
+    Geometry geom( MODE_CYL, Int3D(301,201,1), Vec3D(-0.001,0,0), 0.0001 );
     Solid *s1 = new FuncSolid( solid1 );
     geom.set_solid( 7, s1 );
     Solid *s2 = new FuncSolid( solid2 );
     geom.set_solid( 8, s2 );
     geom.set_boundary( 1, Bound(BOUND_DIRICHLET,  0.0 ) );
-    geom.set_boundary( 2, Bound(BOUND_DIRICHLET, +4.0e3) );
+    geom.set_boundary( 2, Bound(BOUND_DIRICHLET, +6.0e3) );
     geom.set_boundary( 3, Bound(BOUND_NEUMANN,    0.0) );
     geom.set_boundary( 4, Bound(BOUND_NEUMANN,    0.0) );
     geom.set_boundary( 7, Bound(BOUND_DIRICHLET,  0.0)  );
-    geom.set_boundary( 8, Bound(BOUND_DIRICHLET, +4.0e3) );
+    geom.set_boundary( 8, Bound(BOUND_DIRICHLET, +6.0e3) );
     geom.build_mesh();
 
     EpotProblem p;
@@ -75,28 +77,28 @@ void test( int argc, char **argv )
     BiCGSTABSolver solver;
     p.set_solver( solver );
 
-    VectorField bfield;
+    MeshVectorField bfield;
     EpotEfield efield( geom, epot );
-    field_extrpl_e efldextrpl[6] = {FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE, 
-				    FIELD_MIRROR,      FIELD_EXTRAPOLATE,
-				    FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE };
+    field_extrpl_e efldextrpl[6] = { FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE, 
+				     FIELD_MIRROR,      FIELD_EXTRAPOLATE,
+				     FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE };
     efield.set_extrapolation( efldextrpl );
 
     ParticleDataBaseCyl pdb;
-    pdb.set_thread_count( 4 );
     bool pmirror[6] = { false, false, true, false, false, false };
     pdb.set_mirror( pmirror );
     pdb.set_polyint( true );
 
-    for( size_t i = 0; i < 2; i++ ) {
+    for( size_t i = 0; i < 8; i++ ) {
 
 	if( i == 1 ) {
 	    std::vector<double> Ei;
 	    std::vector<double> rhoi;
-	    double rhop = pdb.get_rhosum();
-	    p.set_nsimp_plasma( rhop, 2.5, rhoi, Ei );
+	    Ei.push_back( 1.0 );
+	    double rhoneg = fabs(pdb.get_rhosum());
+	    rhoi.push_back( 0.5*rhoneg );
+	    p.set_nsimp_plasma( 0.5*rhoneg, 10.0, rhoi, Ei );
 	    p.construct( geom );
-	    solver.set_newton_imax( 4 );
 	}
 
 	p.solve( epot, scharge );
@@ -106,41 +108,37 @@ void test( int argc, char **argv )
 	// 1 mA total -> 35.37 A/m2
 	pdb.add_2d_beam_with_energy( 5000, -35.37, -1.0, 1.0, 
 				     5.0, 0.0, 0.5, 
-				     0.0, 0.0, 
-				     0.0, 0.003 );
+				     -0.001, 0.0, 
+				     -0.001, 0.005 );
 	// e-
 	// 20 mA total -> 707.4 A/m2
 	pdb.add_2d_beam_with_energy( 5000, -707.4, -1.0, 1.0/1836.15, 
 				     5.0, 0.0, 0.5, 
-				     0.0, 0.0, 
-				     0.0, 0.003 );
+				     -0.001, 0.0, 
+				     -0.001, 0.005 );
 	pdb.iterate_trajectories( scharge, efield, bfield, geom );
 
 	/*
-	if( i == 4 ) {
-	    GTKPlotter plotter( argc, argv );
-	    plotter.set_geometry( &geom );
-	    plotter.set_epot( &epot );
-	    plotter.set_scharge( &scharge );
-	    plotter.set_particledatabase( &pdb );
-	    plotter.new_geometry_plot_window();
-	    plotter.run();
-	}
+	GTKPlotter plotter( argc, argv );
+	plotter.set_geometry( &geom );
+	plotter.set_epot( &epot );
+	plotter.set_scharge( &scharge );
+	plotter.set_particledatabase( &pdb );
+	plotter.new_geometry_plot_window();
+	plotter.run();
 	*/
-
-	GeomPlotter gplotter( &geom );
-	gplotter.set_size( 1024, 768 );
-	gplotter.set_epot( &epot );
-	std::vector<double> eqlines;
-	eqlines.push_back( -8.0 );
-	eqlines.push_back( -4.0 );
-	eqlines.push_back( 0.0 );
-	eqlines.push_back( +4.0 );
-	gplotter.set_eqlines_manual( eqlines );
-	gplotter.set_particle_database( &pdb );
-	string filename = (string)"nsimp_plasmacyl_" + to_string(i) + ".png";
-	gplotter.plot_png( filename );
-
     }
+    
+    GeomPlotter gplotter( &geom );
+    gplotter.set_size( 1024, 768 );
+    gplotter.set_epot( &epot );
+    std::vector<double> eqlines;
+    eqlines.push_back( +1.0 );
+    eqlines.push_back( +2.0 );
+    eqlines.push_back( +4.0 );
+    eqlines.push_back( +8.0 );
+    gplotter.set_eqlines_manual( eqlines );
+    gplotter.set_particle_database( &pdb );
+    gplotter.plot_png( "nsimp_plasmacyl.png" );
 }
 
