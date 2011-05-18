23203fde0036af7a7fc4aba7827296638cce5c8a
Taneli Kalvas <tvkalvas@myth.(none)>
2011-05-18 14:18:10 +0200

    Random number generator updates.

--- src/random.cpp
+++ src/random.cpp
@@ -1,8 +1,8 @@
-/*! \file qrandom.cpp
- *  \brief Source code for qrandom.cpp
+/*! \file random.cpp
+ *  \brief %Random number generators
  */
 
-/* Copyright (c) 2005-2009 Taneli Kalvas. All rights reserved.
+/* Copyright (c) 2005-2009,2011 Taneli Kalvas. All rights reserved.
  *
  * You can redistribute this software and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software
@@ -40,43 +40,84 @@
  * permit others to do so.
  */
 
+
+#include <iostream>
 #include <cmath>
-#include "qrandom.hpp"
+#include <gsl/gsl_sf_gamma.h>
+#include "random.hpp"
 #include "error.hpp"
 
 
-const double __rgauss_const_A = 1.0/sqrt(2.0);
-const double __rgauss_const_B = 1.0/sqrt(2.0*M_PI);
+Uniform_Transformation *Uniform_Transformation::copy( void ) const
+{
+    return( new Uniform_Transformation( *this) );
+}
 
 
-/* Subroutine providing cumulative distribution function of gaussian at x. */
-double Random::rgauss_f_func( double x, void *params )
+
+/* ********************************************************************************* */
+
+
+
+const double Gaussian_Transformation::_rgauss_const_A = 1.0/sqrt(2.0);
+const double Gaussian_Transformation::_rgauss_const_B = 1.0/sqrt(2.0*M_PI);
+
+
+Gaussian_Transformation::Gaussian_Transformation()
+{
+    _fdf.f   = &rgauss_f_func;
+    _fdf.df  = &rgauss_df_func;
+    _fdf.fdf = &rgauss_fdf_func;
+    _solver  = gsl_root_fdfsolver_alloc( gsl_root_fdfsolver_newton );
+}
+
+
+Gaussian_Transformation::Gaussian_Transformation( const Gaussian_Transformation &trans )
+{
+    _fdf.f   = &rgauss_f_func;
+    _fdf.df  = &rgauss_df_func;
+    _fdf.fdf = &rgauss_fdf_func;
+    _solver  = gsl_root_fdfsolver_alloc( gsl_root_fdfsolver_newton );
+}
+
+
+Gaussian_Transformation::~Gaussian_Transformation()
+{
+    gsl_root_fdfsolver_free( _solver );
+}
+
+
+double Gaussian_Transformation::rgauss_f_func( double x, void *params )
 {
     double *R = (double *)params;
-    return( 0.5*( 1.0+erf(__rgauss_const_A) ) - *R );
+    return( 0.5*( 1.0+erf(x*_rgauss_const_A) ) - *R );
 }
 
-/* Subroutine providing derivative of cumulative distribution function
- * of gaussian at x. */
-double Random::rgauss_df_func( double x, void *params )
+
+double Gaussian_Transformation::rgauss_df_func( double x, void *params )
 {
-    return( __rgauss_const_B*exp( -x*x*__rgauss_const_A ) );
+    return( _rgauss_const_B*exp( -0.5*x*x ) );
 }
+
 
 /* Subroutine providing cumulative distribution function of gaussian
  * and its derivative at x. */
-void Random::rgauss_fdf_func( double x, void *params, double *f, double *df )
+void Gaussian_Transformation::rgauss_fdf_func( double x, void *params, double *f, double *df )
 {
     double *R = (double *)params;
 
-    *f  = 0.5*( 1.0+erf(x*__rgauss_const_A) ) - *R;
-    *df = __rgauss_const_B*exp( -0.5*x*x );
+    *f  = 0.5*( 1.0+erf(x*_rgauss_const_A) ) - *R;
+    *df = _rgauss_const_B*exp( -0.5*x*x );
 }
 
 
-/* Subroutine to filter uniformly distributed R to gaussian
- * distributed variable x by iterative solver. */
-double Random::transform_gaussian( double R )
+Gaussian_Transformation *Gaussian_Transformation::copy( void ) const
+{
+    return( new Gaussian_Transformation( *this) );
+}
+
+
+double Gaussian_Transformation::transform( double R )
 {
     int iter = 0;
     double x, x0;
@@ -98,22 +139,100 @@ double Random::transform_gaussian( double R )
 }
 
 
-Random::Random( int n )
-    : _N(n)
+
+/* ********************************************************************************* */
+
+
+
+Cosine_Transformation::Cosine_Transformation()
 {
-    _fdf.f   = &rgauss_f_func;
-    _fdf.df  = &rgauss_df_func;
-    _fdf.fdf = &rgauss_fdf_func;
+    _fdf.f   = &rcosine_f_func;
+    _fdf.df  = &rcosine_df_func;
+    _fdf.fdf = &rcosine_fdf_func;
     _solver  = gsl_root_fdfsolver_alloc( gsl_root_fdfsolver_newton );
 }
 
 
-Random::~Random()
+Cosine_Transformation::Cosine_Transformation( const Cosine_Transformation &trans )
+{
+    _fdf.f   = &rcosine_f_func;
+    _fdf.df  = &rcosine_df_func;
+    _fdf.fdf = &rcosine_fdf_func;
+    _solver  = gsl_root_fdfsolver_alloc( gsl_root_fdfsolver_newton );
+}
+
+
+Cosine_Transformation::~Cosine_Transformation()
 {
     gsl_root_fdfsolver_free( _solver );
 }
 
 
+double Cosine_Transformation::rcosine_f_func( double x, void *params )
+{
+    double *R = (double *)params;
+
+    if( x < -1.0 )
+	return( -(*R) );
+    else if( x > 1.0 )
+	return( 1.0 - (*R) );
+    return( 0.5*( 1.0 + x + M_1_PI*sin(x*M_PI) ) - *R );
+}
+
+
+double Cosine_Transformation::rcosine_df_func( double x, void *params )
+{
+    if( x < -1.0 )
+	return( 0.0 );
+    else if( x > 1.0 )
+	return( 0.0 );
+    return( 0.5*( 1.0 + cos(x*M_PI) ) );
+}
+
+
+void Cosine_Transformation::rcosine_fdf_func( double x, void *params, double *f, double *df )
+{
+    double *R = (double *)params;
+
+    if( x < -1.0 ) {
+	*f = -(*R);
+	*df = 0.0;
+    } else if( x > 1.0 ) {
+	*f = 1.0 - (*R);
+	*df = 0.0;
+    } else {
+	*f  = 0.5*( 1.0 + x + M_1_PI*sin(x*M_PI) ) - *R;
+	*df = 0.5*( 1.0 + cos(x*M_PI) );
+    }
+}
+
+
+Cosine_Transformation *Cosine_Transformation::copy( void ) const
+{
+    return( new Cosine_Transformation( *this) );
+}
+
+
+double Cosine_Transformation::transform( double R )
+{
+    int iter = 0;
+    double x, x0;
+
+    x = R - 0.5; // Initial guess
+    _fdf.params = (void *)&R;
+    gsl_root_fdfsolver_set( _solver, &_fdf, x );
+    do {
+        gsl_root_fdfsolver_iterate( _solver );
+        x0 = x;
+        x = gsl_root_fdfsolver_root( _solver );
+        if( fabs(x - x0) < 1e-6 )
+	    break;
+	iter++;
+    } while( iter < 100 );
+    if( iter == 100 )
+        throw( Error( ERROR_LOCATION, "too many iterations" ) );
+    return( x );
+}
 
 
 
@@ -121,8 +240,158 @@ Random::~Random()
 
 
 
-QRandom::QRandom( int n )
-    : Random(n)
+Gamma_Transformation::Gamma_Transformation( double k, double theta )
+{
+    _k = k;
+    _theta = theta;
+    _rgamma_const_A = 1.0 / ( gsl_sf_gamma( _k ) * pow( _theta, _k ) );
+    _rgamma_const_B = 1.0 / _theta;
+
+    _fdf.f   = &rgamma_f_func;
+    _fdf.df  = &rgamma_df_func;
+    _fdf.fdf = &rgamma_fdf_func;
+    _solver  = gsl_root_fdfsolver_alloc( gsl_root_fdfsolver_newton );
+}
+
+
+Gamma_Transformation::Gamma_Transformation( const Gamma_Transformation &trans )
+{
+    _k = trans._k;
+    _theta = trans._theta;
+    _rgamma_const_A = trans._rgamma_const_A;
+    _rgamma_const_B = trans._rgamma_const_B;
+
+    _fdf.f   = &rgamma_f_func;
+    _fdf.df  = &rgamma_df_func;
+    _fdf.fdf = &rgamma_fdf_func;
+    _solver  = gsl_root_fdfsolver_alloc( gsl_root_fdfsolver_newton );
+}
+
+
+Gamma_Transformation::~Gamma_Transformation()
+{
+    gsl_root_fdfsolver_free( _solver );
+}
+
+
+Gamma_Transformation &Gamma_Transformation::operator=( const Gamma_Transformation &trans )
+{
+    _k = trans._k;
+    _theta = trans._theta;
+    _rgamma_const_A = trans._rgamma_const_A;
+    _rgamma_const_B = trans._rgamma_const_B;
+
+    return( *this );
+}
+
+
+double Gamma_Transformation::rgamma_f_func( double x, void *params )
+{
+    Gamma_Transformation *trans = (Gamma_Transformation *)params;
+    double R = trans->_R;
+
+    if( x < 0.0 )
+	return( -R );
+    return( gsl_sf_gamma_inc_P( trans->_k, x * trans->_rgamma_const_B ) - R );
+}
+
+
+double Gamma_Transformation::rgamma_df_func( double x, void *params )
+{
+    Gamma_Transformation *trans = (Gamma_Transformation *)params;
+
+    if( x < 0.0 )
+	return( 0.0 );
+    return( pow( x, trans->_k-1.0 ) * exp( -x * trans->_rgamma_const_B ) * trans->_rgamma_const_A );
+}
+
+
+void Gamma_Transformation::rgamma_fdf_func( double x, void *params, double *f, double *df )
+{
+    Gamma_Transformation *trans = (Gamma_Transformation *)params;
+    double R = trans->_R;
+    
+    if( x < 0.0 ) {
+	*f = -R;
+	*df = 0.0;
+    } else {
+	*f  = gsl_sf_gamma_inc_P( trans->_k, x * trans->_rgamma_const_B ) - R;
+	*df = pow( x, trans->_k-1.0 ) * exp( -x * trans->_rgamma_const_B ) * trans->_rgamma_const_A;
+    }
+}
+
+
+Gamma_Transformation *Gamma_Transformation::copy( void ) const
+{
+    return( new Gamma_Transformation( *this) );
+}
+
+
+double Gamma_Transformation::transform( double R )
+{
+    int iter = 0;
+    double x, x0;
+
+    //std::cout << "Transform( R = " << R << ")\n";
+
+    x = _k*_theta; // Initial guess: mean
+    _R = R;
+    _fdf.params = (void *)this;
+    gsl_root_fdfsolver_set( _solver, &_fdf, x );
+    do {
+        gsl_root_fdfsolver_iterate( _solver );
+        x0 = x;
+	//std::cout << "  iter " << iter << ": ";
+	//std::cout << "  x = " << x << " ";
+        x = gsl_root_fdfsolver_root( _solver );
+	//std::cout << ",  err = " << fabs(x - x0) << "\n";
+        if( fabs(x - x0) < 1e-6 )
+	    break;
+	iter++;
+    } while( iter < 100 );
+    if( iter == 100 )
+        throw( Error( ERROR_LOCATION, "too many iterations" ) );
+    return( x );
+}
+
+
+
+/* ********************************************************************************* */
+
+
+
+Random::Random( size_t N )
+{
+    pthread_mutex_init( &_mutex, NULL );
+    for( size_t a = 0; a < N; a++ )
+	_transformation.push_back( new Uniform_Transformation );
+}
+
+
+Random::~Random()
+{
+    pthread_mutex_destroy( &_mutex );
+    for( size_t a = 0; a < _transformation.size(); a++ )
+	delete( _transformation[a] );
+}
+
+
+void Random::set_transformation( size_t i, const Random_Variate_Transformation &trans )
+{
+    if( i >= _transformation.size() )
+	throw( ErrorRange( ERROR_LOCATION, i, _transformation.size() ) );
+
+    delete( _transformation[i] );
+    _transformation[i] = trans.copy();
+}
+
+
+/* ********************************************************************************* */
+
+
+
+QRandom::QRandom( size_t N )
+    : Random(N), _N(N)
 {
     _qrng = gsl_qrng_alloc( gsl_qrng_sobol, _N );
 }
@@ -134,31 +403,13 @@ QRandom::~QRandom()
 }
 
 
-void QRandom::get_gaussian( double *x )
+void QRandom::get( double *x ) const
 {
-    int i;
-
+    pthread_mutex_lock( &_mutex );
     gsl_qrng_get( _qrng, x );
-    for( i = 0; i < _N; i++ )
-	x[i] = transform_gaussian( x[i] );
-}
-
-
-void QRandom::get_part_gaussian( bool *gaussian, double *x )
-{
-    int i;
-
-    gsl_qrng_get( _qrng, x );
-    for( i = 0; i < _N; i++ ) {
-	if( gaussian[i] )
-	    x[i] = transform_gaussian( x[i] );
-    }
-}
-
-
-void QRandom::get( double *x )
-{
-    gsl_qrng_get( _qrng, x );
+    for( size_t a = 0; a < _N; a++ )
+	x[a] = _transformation[a]->transform( x[a] );
+    pthread_mutex_unlock( &_mutex );
 }
 
 
@@ -168,8 +419,8 @@ void QRandom::get( double *x )
 
 
 
-MTRandom::MTRandom( int n )
-    : Random(n)
+MTRandom::MTRandom( size_t N )
+    : Random(N), _N(N)
 {
     _rng = gsl_rng_alloc( gsl_rng_mt19937 );
 }
@@ -181,49 +432,10 @@ MTRandom::~MTRandom()
 }
 
 
-void MTRandom::get_gaussian( double *x )
+void MTRandom::get( double *x ) const
 {
-    for( int i = 0; i < _N; i++ )
-	x[i] = gsl_rng_uniform( _rng );
-
-    for( int i = 0; i < _N; i++ )
-	x[i] = transform_gaussian( x[i] );
+    pthread_mutex_lock( &_mutex );
+    for( size_t a = 0; a < _N; a++ )
+	x[a] = _transformation[a]->transform( gsl_rng_uniform( _rng ) );
+    pthread_mutex_unlock( &_mutex );
 }
-
-
-void MTRandom::get_part_gaussian( bool *gaussian, double *x )
-{
-    for( int i = 0; i < _N; i++ )
-	x[i] = gsl_rng_uniform( _rng );
-
-    for( int i = 0; i < _N; i++ ) {
-	if( gaussian[i] )
-	    x[i] = transform_gaussian( x[i] );
-    }
-}
-
-
-void MTRandom::get( double *x )
-{
-    for( int i = 0; i < _N; i++ )
-	x[i] = gsl_rng_uniform( _rng );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/random.hpp
+++ src/random.hpp
@@ -1,4 +1,4 @@
-/*! \file qrandom.hpp
+/*! \file random.hpp
  *  \brief %Random number generators
  */
 
@@ -40,124 +40,283 @@
  * permit others to do so.
  */
 
-#ifndef QRANDOM_HPP
-#define QRANDOM_HPP 1
+#ifndef RANDOM_HPP
+#define RANDOM_HPP 1
 
 
+#include <stdint.h>
+#include <pthread.h>
+#include <vector>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_qrng.h>
 #include <gsl/gsl_roots.h>
 
 
-/*! \brief %Random number generator for N dimensions.
- *
- *  This RNG can produce random numbers in N independent
- *  dimensions. RNG includes functions to return uniformly
- *  distributed numbers between 0 and 1 and numbers from a gaussian
- *  distribution.
+
+/*! \brief Base class for non-uniform random variate transformation.
  */
-class Random {
+class Random_Variate_Transformation {
+
+public:
+
+    /*! \brief Virtual destructor.
+     */
+    virtual ~Random_Variate_Transformation() {}
+
+    /*! \brief Return a newly allocated copy of object.
+     */
+    virtual Random_Variate_Transformation *copy( void ) const = 0;
+
+    /*! \brief Returns number from distribution, transformed from
+     *  uniformly distributed R, where 0 <= R <= 1.
+     */
+    virtual double transform( double R ) = 0;
+};
+
+
+
+/*! \brief Uniform transformation.
+ *
+ *  Unity transformation.
+ */
+class Uniform_Transformation : public Random_Variate_Transformation {
+
+public:
+
+    /*! \brief Constructor.
+     */
+    Uniform_Transformation() {}
+
+    /*! \brief Copy constructor.
+     */
+    Uniform_Transformation( const Uniform_Transformation &trans ) {}
+
+    /*! \brief Virtual destructor.
+     */
+    virtual ~Uniform_Transformation() {}
+
+    /*! \brief Assignment
+     */
+    Uniform_Transformation &operator=( const Uniform_Transformation &trans ) { return( *this ); }
+
+    /*! \brief Return a newly allocated copy of object.
+     */
+    virtual Uniform_Transformation *copy( void ) const;
+
+    /*! \brief Returns the random variate with no transformation.
+     */
+    virtual double transform( double R ) { return( R ); }
+};
+
+
+
+/*! \brief Random variate transformation for Gaussian distribution.
+ */
+class Gaussian_Transformation : public Random_Variate_Transformation {
 
     gsl_function_fdf    _fdf;      /*!< \brief Function to solve for gaussian transformation. */
     gsl_root_fdfsolver *_solver;   /*!< \brief Solver for gaussian transformation. */
+
+    static const double _rgauss_const_A;
+    static const double _rgauss_const_B;
 
     static double rgauss_f_func( double x, void *params );
     static double rgauss_df_func( double x, void *params );
     static void rgauss_fdf_func( double x, void *params, double *f, double *df );
 
-protected:
+public:
 
     /*! \brief Constructor.
      */
-    Random( int n );
+    Gaussian_Transformation();
 
-    /*! \brief Prevent copying
+    /*! \brief Copy constructor.
      */
-    Random( const Random &rng ) {}
+    Gaussian_Transformation( const Gaussian_Transformation &trans );
 
-    int                 _N;        /*!< \brief Number of dimensions for RNG. */
-
-    /*! \brief Returns number from gaussian distribution, transformed
-     *  from uniformly distributed R, where 0 <= R <= 1.
+    /*! \brief Virtual destructor.
      */
-    double transform_gaussian( double R );
+    virtual ~Gaussian_Transformation();
+
+    /*! \brief Assignment
+     */
+    Gaussian_Transformation &operator=( const Gaussian_Transformation &trans ) { return( *this ); }
+
+    /*! \brief Return a newly allocated copy of object.
+     */
+    virtual Gaussian_Transformation *copy( void ) const;
+
+    /*! \brief Returns number from distribution, transformed from
+     *  uniformly distributed R, where 0 <= R <= 1.
+     */
+    virtual double transform( double R );
+};
+
+
+
+/*! \brief Random variate transformation for raised cosine distribution.
+ */
+class Cosine_Transformation : public Random_Variate_Transformation {
+
+    gsl_function_fdf    _fdf;      /*!< \brief Function to solve for cosine transformation. */
+    gsl_root_fdfsolver *_solver;   /*!< \brief Solver for cosine transformation. */
+
+    static double rcosine_f_func( double x, void *params );
+    static double rcosine_df_func( double x, void *params );
+    static void rcosine_fdf_func( double x, void *params, double *f, double *df );
 
 public:
+
+    /*! \brief Constructor.
+     */
+    Cosine_Transformation();
+
+    /*! \brief Copy constructor.
+     */
+    Cosine_Transformation( const Cosine_Transformation &trans );
+
+    /*! \brief Virtual destructor.
+     */
+    virtual ~Cosine_Transformation();
+
+    /*! \brief Assignment
+     */
+    Cosine_Transformation &operator=( const Cosine_Transformation &trans ) { return( *this ); }
+
+    /*! \brief Return a newly allocated copy of object.
+     */
+    virtual Cosine_Transformation *copy( void ) const;
+
+    /*! \brief Returns number from distribution, transformed from
+     *  uniformly distributed R, where 0 <= R <= 1.
+     */
+    virtual double transform( double R );
+};
+
+
+
+/*! \brief Random variate transformation for gamma distribution.
+ */
+class Gamma_Transformation : public Random_Variate_Transformation {
+
+    gsl_function_fdf    _fdf;      /*!< \brief Function to solve for cosine transformation. */
+    gsl_root_fdfsolver *_solver;   /*!< \brief Solver for cosine transformation. */
+    
+    double _R;
+    double _k;
+    double _theta;
+    double _rgamma_const_A;
+    double _rgamma_const_B;
+
+    static double rgamma_f_func( double x, void *params );
+    static double rgamma_df_func( double x, void *params );
+    static void rgamma_fdf_func( double x, void *params, double *f, double *df );
+
+public:
+
+    /*! \brief Constructor.
+     */
+    Gamma_Transformation( double k, double theta );
+
+    /*! \brief Copy constructor.
+     */
+    Gamma_Transformation( const Gamma_Transformation &trans );
+
+    /*! \brief Virtual destructor.
+     */
+    virtual ~Gamma_Transformation();
+
+    /*! \brief Assignment
+     */
+    Gamma_Transformation &operator=( const Gamma_Transformation &trans );
+
+    /*! \brief Return a newly allocated copy of object.
+     */
+    virtual Gamma_Transformation *copy( void ) const;
+
+    /*! \brief Returns number from distribution, transformed from
+     *  uniformly distributed R, where 0 <= R <= 1.
+     */
+    virtual double transform( double R );
+};
+
+
+
+/*! \brief %Random number generator for N dimensions.
+ *
+ *  This random number generator can produce random numbers in N
+ *  independent dimensions. The RNG can be set to either return
+ *  uniformly distributed numbers between 0 and 1 and numbers
+ *  transformed by set transformations. The transformations can be set
+ *  independently for each dimension.
+ *
+ *  Random number generators should be mutex protected.
+ */
+class Random {
+
+protected:
+
+    mutable pthread_mutex_t _mutex;
+    std::vector<Random_Variate_Transformation *> _transformation;
+
+public:
+
+    /*! \brief Constructor for \a N dimensional RNG.
+     */
+    Random( size_t N );
 
     /*! \brief Destructor.
      */
     virtual ~Random();
 
-    /*! \brief Get gaussian random numbers.
+    /*! \brief Set random variate transformation for dimension \a i.
      *
-     *  Get next sampling from gaussian quasi random number generator
-     *  to \a x with standard deviation of 1. Array \a x must have
-     *  space for \a N numbers. */
-    virtual void get_gaussian( double *x ) = 0;
-    
-    /*! \brief Get uniform and gaussian random numbers.
-     * 
-     *  Get next sampling from quasi random number generator to \a x
-     *  with dimensions marked (to true) in list gaussian mapped
-     *  to gaussian distribution with standard deviation of 1. Rest of
-     *  the dimensions are linear. Array x must have space for \a N
-     *  numbers. */
-    virtual void get_part_gaussian( bool *gaussian, double *x ) = 0;
+     *  All transformations default to Uniform_Transformation.
+     */
+    void set_transformation( size_t i, const Random_Variate_Transformation &trans );
 
-    /*! \brief Get uniform random numbers.
+    /*! \brief Get a set of random numbers.
      * 
-     *  Get next sampling from quasi random number generator to \a x.
-     *  Array x must have space for \a N numbers. */
-    virtual void get( double *x ) = 0;
-
+     *  Get next sampling from random number generator to array \a x.
+     *  The array x must have space for \a N numbers. 
+     */
+    virtual void get( double *x ) const = 0;
 };
 
 
 /*! \brief Quasi random number generator for N dimensions.
  *
- *  This QRNG can produce quasi random numbers in N independent
- *  dimensions using the Sobol sequence from GSL library. QRNG
- *  includes functions to return uniformly distributed numbers between
- *  0 and 1 and numbers from a gaussian distribution.
+ *  This random number generator can produce quasi random numbers in N
+ *  independent dimensions using the Sobol sequence. Implementation
+ *  from GSL library.
+ *
+ *  Mutex protected.
  */
 class QRandom : public Random {
-    gsl_qrng           *_qrng;     /*!< \brief Random number generator from gsl. */
 
-    /*! \brief Prevent copying
+    size_t    _N;        /*!< \brief Number of dimensions. */
+    gsl_qrng *_qrng;     /*!< \brief Random number generator from gsl. */
+
+    /*! \brief Prevent copying.
      */
-    QRandom( const QRandom &qrng ) : Random(qrng) {}
+    QRandom( const QRandom &qrng ) : Random(0) {}
 
 public:
 
-    /*! \brief Constructor for QRNG in \a N independent dimensions.
+    /*! \brief Constructor for random number generator in \a N independent dimensions.
      */
-    QRandom( int n );
+    QRandom( size_t N );
 
-    /*! \brief Destructor.
+    /*! \brief Virtual destructor.
      */
-    ~QRandom();
+    virtual ~QRandom();
 
-    /*! \brief Get gaussian random numbers.
-     *
-     *  Get next sampling from gaussian quasi random number generator
-     *  to \a x with standard deviation of 1. Array \a x must have
-     *  space for \a N numbers. */
-    virtual void get_gaussian( double *x );
-    
-    /*! \brief Get uniform and gaussian random numbers.
+    /*! \brief Get a set of random numbers.
      * 
-     *  Get next sampling from quasi random number generator to \a x
-     *  with dimensions marked (to true) in list gaussian mapped
-     *  to gaussian distribution with standard deviation of 1. Rest of
-     *  the dimensions are linear. Array x must have space for \a N
-     *  numbers. */
-    virtual void get_part_gaussian( bool *gaussian, double *x );
-
-    /*! \brief Get uniform random numbers.
-     * 
-     *  Get next sampling from quasi random number generator to \a x.
-     *  Array x must have space for \a N numbers. */
-    virtual void get( double *x );
+     *  Get next sampling from random number generator to array \a x.
+     *  The array x must have space for \a N numbers. 
+     */
+    virtual void get( double *x ) const;
 };
 
 
@@ -167,50 +326,35 @@ public:
  *  dimensions. RNG includes functions to return uniformly distributed
  *  numbers between 0 and 1 and numbers from a gaussian distribution.
  *  Based on MT19937 generator from gsl.
+ *
+ *  Mutex protected.
  */
 class MTRandom : public Random {
-    gsl_rng           *_rng;     /*!< \brief Random number generator from gsl. */
+
+    size_t    _N;        /*!< \brief Number of dimensions. */
+    gsl_rng  *_rng;      /*!< \brief Random number generator from gsl. */
 
     /*! \brief Prevent copying
      */
-    MTRandom( const MTRandom &rng ) : Random(rng) {}
+    MTRandom( const MTRandom &rng ) : Random(0) {}
 
 public:
 
-    /*! \brief Constructor for RNG in \a N independent dimensions.
+    /*! \brief Constructor for random number generator in \a N independent dimensions.
      */
-    MTRandom( int n );
+    MTRandom( size_t N );
 
-    /*! \brief Destructor.
+    /*! \brief Virtual destructor.
      */
-    ~MTRandom();
+    virtual ~MTRandom();
 
-    /*! \brief Get gaussian random numbers.
-     *
-     *  Get next sampling from gaussian quasi random number generator
-     *  to \a x with standard deviation of 1. Array \a x must have
-     *  space for \a N numbers. */
-    virtual void get_gaussian( double *x );
-    
-    /*! \brief Get uniform and gaussian random numbers.
+    /*! \brief Get a set of random numbers.
      * 
-     *  Get next sampling from quasi random number generator to \a x
-     *  with dimensions marked (to true) in list gaussian mapped
-     *  to gaussian distribution with standard deviation of 1. Rest of
-     *  the dimensions are linear. Array x must have space for \a N
-     *  numbers. */
-    virtual void get_part_gaussian( bool *gaussian, double *x );
-
-    /*! \brief Get uniform random numbers.
-     * 
-     *  Get next sampling from quasi random number generator to \a x.
-     *  Array x must have space for \a N numbers. */
-    virtual void get( double *x );
+     *  Get next sampling from random number generator to array \a x.
+     *  The array x must have space for \a N numbers. 
+     */
+    virtual void get( double *x ) const;
 };
 
 
 #endif
-
-
-
-
