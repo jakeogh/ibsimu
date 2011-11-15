d8eda68e8e97f2ed52d89d2a41bb215fae3c9ae2
Taneli Kalvas <taneli.kalvas@jyu.fi>
2011-11-15 22:49:08 +0200

    Made min/max evaluators for general vector fields.

--- src/vectorfield.cpp
+++ src/vectorfield.cpp
@@ -1,8 +1,8 @@
-/*! \file vectorfield.hpp
- *  \brief Vector field base
+/*! \file vectorfield.cpp
+ *  \brief %Vector field base
  */
 
-/* Copyright (c) 2005-2011 Taneli Kalvas. All rights reserved.
+/* Copyright (c) 2011 Taneli Kalvas. All rights reserved.
  *
  * You can redistribute this software and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software
@@ -40,44 +40,60 @@
  * permit others to do so.
  */
 
-#ifndef VECTORFIELD_HPP
-#define VECTORFIELD_HPP 1
+
+#include <limits>
+#include "vectorfield.hpp"
 
 
-#include "mesh.hpp"
-#include "vec3d.hpp"
+void VectorField::get_minmax( const Mesh &mesh, double &min, double &max ) const
+{
+    min = std::numeric_limits<double>::infinity();
+    max = -std::numeric_limits<double>::infinity();
+
+    for( size_t k = 0; k < mesh.size(2); k++ ) {
+	for( size_t j = 0; j < mesh.size(1); j++ ) {
+	    for( size_t i = 0; i < mesh.size(0); i++ ) {
+
+		Vec3D x( mesh.origo(0)+i*mesh.h(),
+			 mesh.origo(1)+i*mesh.h(),
+			 mesh.origo(2)+i*mesh.h() );
+		double val = (*this)( x ).ssqr();
+		if( val < min )
+		    min = val;
+		if( val > max )
+		    max = val;
+	    }
+	}
+    }
+    min = sqrt( min );
+    max = sqrt( max );
+}
 
 
-/*! \brief Abstract base class for vector field.
- *
- */
-class VectorField {
+void VectorField::get_minmax( const Mesh &mesh, Vec3D &min, Vec3D &max ) const
+{
+    min = Vec3D( std::numeric_limits<double>::infinity(),
+		 std::numeric_limits<double>::infinity(),
+		 std::numeric_limits<double>::infinity() );
+    max = Vec3D( -std::numeric_limits<double>::infinity(),
+		 -std::numeric_limits<double>::infinity(),
+		 -std::numeric_limits<double>::infinity() );
 
-public:
+    for( size_t k = 0; k < mesh.size(2); k++ ) {
+	for( size_t j = 0; j < mesh.size(1); j++ ) {
+	    for( size_t i = 0; i < mesh.size(0); i++ ) {
 
-    /*! \brief Virtual destructor.
-     */
-    virtual ~VectorField() {}
-
-    /*! \brief Operator for getting interpolated field value
-     *  at \a x.
-     */
-    virtual const Vec3D operator()( const Vec3D x ) const = 0;
-
-    /*! \brief Search minimum and maximum vector length values of
-     *  vector field at \a mesh nodes
-     */
-    void get_minmax( const Mesh &mesh, double &min, double &max ) const;
-
-    /*! \brief Search minimum and maximum for each vector component at
-        \a mesh nodes.
-     *
-     *  Return vector min with minimum components found and vector max
-     *  with maximum components found.
-     */
-    void get_minmax( const Mesh &mesh, Vec3D &min, Vec3D &max ) const;
-};
-
-
-#endif
-
+		Vec3D x( mesh.origo(0)+i*mesh.h(),
+			 mesh.origo(1)+i*mesh.h(),
+			 mesh.origo(2)+i*mesh.h() );
+		Vec3D val = (*this)( x ).ssqr();
+		for( size_t b = 0; b < 3; b++ ) {
+		    if( val[b] < min[b] )
+			min[b] = val[b];
+		    if( val[b] > max[b] )
+			max[b] = val[b];
+		}
+	    }
+	}
+    }
+}
